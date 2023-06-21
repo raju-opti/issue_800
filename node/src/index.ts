9bd87be08b2e1d2cb016facd5c8fce8e46642533
Raju Ahmed <raju.ahmed@optimizely.com>
2023-06-21 22:45:50 +0600

    update

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,7 +2,6 @@ import optimizelySdk from '@optimizely/optimizely-sdk';
 
 const loadOptimizely = async () => {
     try {
-      // Instantiate Optimizely w/SDK Key & LogLevel @ DEBUG
       const optimizelyInstance = optimizelySdk.createInstance({
         logLevel: 'error',
         sdkKey: 'your-sdk-key',
@@ -13,6 +12,8 @@ const loadOptimizely = async () => {
       }
 
       const { success, reason } = await optimizelyInstance.onReady();
+
+      console.log(success, reason);
 
       const userId = 'matjaz-user-1';
       const user = optimizelyInstance.createUserContext(userId);
@@ -26,53 +27,6 @@ const loadOptimizely = async () => {
       const decision = user.decideAll();
 
       console.log(decision);
-    //   console.log(`Qualified segments for user (${user.getUserId()}) before fetch:`);
-    //   console.log(user.qualifiedSegments); // Should be empty
-
-    //   await user.fetchQualifiedSegments(); // Fetch qualifiedSegments for user
-
-    //   console.log(`Qualified segments for user (${user.getUserId()}) after fetch:`);
-    //   console.log(user.qualifiedSegments); // Return updated qualifiedSegments
-
-    //   // Create Custom User (uses FS_USER_ID)
-    //   const testUserId = 'test_user_01';
-    //   const testUser = optimizelyInstance.createUserContext(testUserId, {
-    //     favorite_color: 'red',
-    //   });
-
-    //   if (!testUser) {
-    //     throw new Error(
-    //       `Error: Unable to create new Optimizely User Context for test user (${testUserId}).`,
-    //     );
-    //   }
-
-    //   // Tie testUserId with email identifier...
-    //   console.log('Sending custom ODP event...');
-    //   optimizelyInstance.sendOdpEvent(
-    //     'associate_email',
-    //     undefined,
-    //     new Map([['email', 'john.nguyen@optimizely.com']]),
-    //   );
-
-    //   // Fetch Qualified Segments
-    //   console.log('Fetching qualified segments...');
-    //   const fetchedQualifiedSegments = await testUser.fetchQualifiedSegments();
-
-    //   if (!fetchedQualifiedSegments) {
-    //     console.warn(
-    //       `Warning: Unable to fetch qualified segments for user (${testUser.getUserId()})`,
-    //     );
-    //   } else {
-    //     console.log(`Fetched segments for user (${testUser.getUserId()})`);
-    //     console.log(`User's qualified segments: [${user.qualifiedSegments}]`);
-    //   }
-
-    //   // Make Decision
-    //   const promotionDecision = testUser.decide('promotion');
-    //   console.log(`Decision for (promotion) made for user (${testUser.getUserId()}):`);
-    //   console.log(promotionDecision);
-
-    //   setOptimizelyLoaded(success);
     } catch (e) {
       console.error('Unable to load Optimizely.');
       console.error(e);
